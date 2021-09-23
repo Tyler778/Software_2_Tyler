ff9a321a2531e624c24379a89fe2efd54ef6293f
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2021-09-23 01:49:14 -0400

    modification report gui created

--- src/controller/ModificationReportController.java
+++ src/controller/ModificationReportController.java
@@ -5,9 +5,21 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import model.Appointment;
 
 /**
  * FXML Controller class
@@ -15,13 +27,43 @@ import javafx.fxml.Initializable;
  * @author tyler
  */
 public class ModificationReportController implements Initializable {
+    Stage stage;
+    Parent scene;
+
+    @FXML
+    private TableView<Appointment> appointmentTable;
+    @FXML
+    private TableColumn<Appointment, Integer> apptIDCol;
+    @FXML
+    private TableColumn<Appointment, String> titleCol;
+    @FXML
+    private TableColumn<Appointment, String> descCol;
+    @FXML
+    private TableColumn<Appointment, String> locationCol;
+    @FXML
+    private TableColumn<Appointment, String> typeCol;
+    @FXML
+    private TableColumn<Appointment, LocalDateTime> startCol;
+    @FXML
+    private TableColumn<Appointment, LocalDateTime> endCol;
+    @FXML
+    private TableColumn<Appointment, String> countryCol;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
         // TODO
     }    
+
+    @FXML
+    private void onActionMainMenu(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/SchedulingHome.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
     
 }
