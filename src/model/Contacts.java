47aeec7903ab4cfc449e2d11cb93ac4b671056aa
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2021-09-25 01:36:37 -0400

    removed Credentials and added javadoc comments to Contacts

--- src/model/Contacts.java
+++ src/model/Contacts.java
@@ -13,33 +13,49 @@ public class Contacts {
     private int id;
     private String name;
     private String email;
-
+    /**
+     * Constructor for the Contacts Class
+     * @param id
+     * @param name
+     * @param email 
+     */
     public Contacts(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
-
+    /**
+     * Return the ID of the Contact object
+     * @return 
+     */
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**
+     * Return the Name of the Contact object
+     * @return 
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Set the Name of the Contact object
+     * @param name 
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Return the email of the Contact object
+     * @return 
+     */
     public String getEmail() {
         return email;
     }
-
+    /**
+     * Set the email of the Contact object
+     * @param email 
+     */
     public void setEmail(String email) {
         this.email = email;
     }
